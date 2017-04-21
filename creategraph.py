1710f9b19502f1e3388f8d76e8be8113ddaf5993
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-21 12:09:41 -0600

    Dry run through the vertices and edges. Generating filenames

--- creategraph.py
+++ creategraph.py
@@ -6,10 +6,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("output", help="output file prefix", type=str)
     parser.add_argument("n_start", help="number of initial vertices", type=int)
-    parser.add_argument("n_iter", help="number of iterations for vertices", type=int)
+    parser.add_argument("n_finish", help="number of final vertices", type=int)
     parser.add_argument("n_step", help="number of vertices to increment each iteration", type=int)
     parser.add_argument("m_start", help="number of initial edges", type=int)
-    parser.add_argument("m_iter", help="number of iterations for edges", type=int)
+    parser.add_argument("m_finish", help="number of final edges", type=int)
     parser.add_argument("m_step", help="number of edges to increment each iteration", type=int)
     parser.add_argument("-s", "--seed", help="seed value for randomgraph", type=int)
 
@@ -18,11 +18,11 @@ def main():
     print(" Output file prefix: ", args.output)
 
     print(" N start: ", args.n_start)
-    print(" N iter: ", args.n_iter)
+    print(" N finish: ", args.n_finish)
     print(" N step: ", args.n_step)
 
     print(" M start: ", args.m_start)
-    print(" M iter: ", args.m_iter)
+    print(" M finish: ", args.m_finish)
     print(" M step: ", args.m_step)
 
     if args.seed:
@@ -30,12 +30,15 @@ def main():
     else:
         print(" Seed NOT present")
 
+    # randomgraph_result = subprocess.run(["./randomgraph", str(args.n_start), str(args.m_start)],
+    #                                     encoding='utf-8',
+    #                                     stdout=subprocess.PIPE)
+    # print(randomgraph_result.stdout)
 
-
-    randomgraph_result = subprocess.run(["./randomgraph", str(args.n_start), str(args.m_start)],
-                                        encoding='utf-8',
-                                        stdout=subprocess.PIPE)
-    print(randomgraph_result.stdout)
+    for n in range(args.n_start, args.n_finish, args.n_step):
+        for m in range(args.m_start, args.m_finish, args.m_step):
+            filename = "{0}_{1}_{2}.out".format(args.output, n, m)
+            print("{0} {1} Filename: {2}".format(n, m, filename))
 
 
 if __name__ == '__main__':
