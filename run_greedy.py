27de9324b084cbf19e773cf789873a2b25bef655
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-23 17:49:14 -0600

    filtering out the files with ".graph" extension

--- run_greedy.py
+++ run_greedy.py
@@ -2,19 +2,28 @@ import os
 import subprocess
 
 
-def main():
-    graph_files = sorted(os.listdir('outputs/'))
-    for graph_file in graph_files:
-        graph_filename = os.path.join('outputs/', graph_file)
-        op_filename = graph_file[:-4] + ".greedy"
+def is_graph_file(filename):
+    if filename.endswith(".graph"):
+        return True
+    else:
+        return False
 
-        op_filepath = os.path.join('outputs/', op_filename)
-        greedy_result = subprocess.run(["./greedy", graph_filename],
-                                            encoding='utf-8',
-                                            stdout=subprocess.PIPE)
-        print(op_filename)
-        with open(op_filepath, mode='w', encoding='utf-8') as output_file:
-            output_file.write(greedy_result.stdout)
+def main():
+    # grab the graph files -- the ones with .graph extension
+    graph_files = filter(is_graph_file, sorted(os.listdir('outputs/')) )
+
+    for graph_file in graph_files:
+        print(graph_file)
+        # graph_filename = os.path.join('outputs/', graph_file)
+        # op_filename = graph_file[:-4] + ".greedy"
+        #
+        # op_filepath = os.path.join('outputs/', op_filename)
+        # greedy_result = subprocess.run(["./greedy", graph_filename],
+        #                                     encoding='utf-8',
+        #                                     stdout=subprocess.PIPE)
+        # print(op_filename)
+        # with open(op_filepath, mode='w', encoding='utf-8') as output_file:
+        #     output_file.write(greedy_result.stdout)
 
 
 if __name__ == '__main__':
