86983a8c6afc7d38557b156d216952c69963c66e
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-23 17:52:59 -0600

    running and putting output of ./greedy to files with .greedy extension

--- run_greedy.py
+++ run_greedy.py
@@ -13,17 +13,16 @@ def main():
     graph_files = filter(is_graph_file, sorted(os.listdir('outputs/')) )
 
     for graph_file in graph_files:
-        print(graph_file)
-        # graph_filename = os.path.join('outputs/', graph_file)
-        # op_filename = graph_file[:-4] + ".greedy"
-        #
-        # op_filepath = os.path.join('outputs/', op_filename)
-        # greedy_result = subprocess.run(["./greedy", graph_filename],
-        #                                     encoding='utf-8',
-        #                                     stdout=subprocess.PIPE)
-        # print(op_filename)
-        # with open(op_filepath, mode='w', encoding='utf-8') as output_file:
-        #     output_file.write(greedy_result.stdout)
+        # print(graph_file)
+        graph_filename = os.path.join('outputs/', graph_file)
+        op_filename = graph_file[:-6] + ".greedy"  # set the .greedy extension
+        op_filepath = os.path.join('outputs/', op_filename)
+        greedy_result = subprocess.run(["./greedy", graph_filename],
+                                            encoding='utf-8',
+                                            stdout=subprocess.PIPE)
+        print(op_filename)
+        with open(op_filepath, mode='w', encoding='utf-8') as output_file:
+            output_file.write(greedy_result.stdout)
 
 
 if __name__ == '__main__':
