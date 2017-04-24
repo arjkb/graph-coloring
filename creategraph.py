6633f038d041a98d738b278f4c80f437bc93ca06
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-23 18:31:42 -0600

    added an optional "verbose" parameter. Showing the total counts summary at the end

--- creategraph.py
+++ creategraph.py
@@ -12,6 +12,7 @@ def main():
     parser.add_argument("n_finish", help="number of final vertices", type=int)
     parser.add_argument("n_step", help="number of vertices to increment each iteration", type=int)
     parser.add_argument("-r", "--runs", help="number of times the entire program must be run", type=int)
+    parser.add_argument("--verbose", help="show all output", action="store_true")
     # parser.add_argument("m_start", help="number of initial edges", type=int)
     # # parser.add_argument("m_finish", help="number of final edges", type=int)
     # parser.add_argument("m_step", help="number of edges to increment each iteration", type=int)
@@ -40,20 +41,26 @@ def main():
     else:
         total_runs = 1
 
-    count = 1
-    for run_count in range(args.runs):
+    print(" Creating graphs...")
+
+    count = 0
+    for run_count in range(total_runs):
         filename_prefix = args.output + str(run_count)
+
         for n in range(args.n_start, args.n_finish, args.n_step):
             m_max = int(n*(n-1)/2)
             m_s = int(0.30 * m_max)
             m_e = int(0.60 * m_max)
-            print(" For n = {}, m_start={}, m_end={}".format(n, m_s, m_e))
+            if args.verbose:
+                print(" For n = {}, m_start={}, m_end={}".format(n, m_s, m_e))
 
             for m in range(m_s, m_e, 1):
                 count += 1
                 filename = "{0}_{1}_{2}.graph".format(filename_prefix, n, m)
                 filepath = os.path.join('outputs/', filename)
-                print(" Count: {} ({}, {}) Filename: {}".format(count, n, m, filename))
+
+                if args.verbose:
+                    print(" Count: {} ({}, {}) Filename: {}".format(count, n, m, filename))
 
                 randomgraph_result = subprocess.run(["./randomgraph", str(n), str(m)],
                                                     encoding='utf-8',
@@ -62,6 +69,12 @@ def main():
 
                 with open(filepath, mode='w', encoding='utf-8') as output_file:
                     output_file.write(randomgraph_result.stdout)
+
+
+    print("\n Created {} sets of {} graphs in outputs/ directory"
+                                    .format(total_runs, count/total_runs))
+    print(" Total number of graphs: {}".format(count))
+    print()
 
 
 if __name__ == '__main__':
