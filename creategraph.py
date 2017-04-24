82b0d29c571f9fd6d4451b0f29f6c02c21bc6283
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-23 18:08:25 -0600

    adding optional argument to specify the number of runs

--- creategraph.py
+++ creategraph.py
@@ -11,10 +11,11 @@ def main():
     parser.add_argument("n_start", help="number of initial vertices", type=int)
     parser.add_argument("n_finish", help="number of final vertices", type=int)
     parser.add_argument("n_step", help="number of vertices to increment each iteration", type=int)
+    parser.add_argument("-r", "--runs", help="number of times the entire program must be run", type=int)
     # parser.add_argument("m_start", help="number of initial edges", type=int)
     # # parser.add_argument("m_finish", help="number of final edges", type=int)
     # parser.add_argument("m_step", help="number of edges to increment each iteration", type=int)
-    parser.add_argument("-s", "--seed", help="seed value for randomgraph", type=int)
+    # parser.add_argument("-s", "--seed", help="seed value for randomgraph", type=int)
 
     args = parser.parse_args()
 
@@ -28,31 +29,39 @@ def main():
     # # print(" M finish: ", args.m_finish)
     # print(" M step: ", args.m_step)
 
-    if args.seed:
-        print(" Seed: ", args.seed)
+    # if args.seed:
+    #     print(" Seed: ", args.seed)
+    # else:
+    #     print(" Seed NOT present")
+
+    total_runs = 1
+    if args.runs:
+        total_runs = args.runs
     else:
-        print(" Seed NOT present")
+        total_runs = 1
 
     count = 1
-    for n in range(args.n_start, args.n_finish, args.n_step):
-        m_max = int(n*(n-1)/2)
-        m_s = int(0.30 * m_max)
-        m_e = int(0.60 * m_max)
-        print(" For n = {}, m_start={}, m_end={}".format(n, m_s, m_e))
+    for run_count in range(args.runs):
+        filename_prefix = args.output + str(run_count)
+        for n in range(args.n_start, args.n_finish, args.n_step):
+            m_max = int(n*(n-1)/2)
+            m_s = int(0.30 * m_max)
+            m_e = int(0.60 * m_max)
+            print(" For n = {}, m_start={}, m_end={}".format(n, m_s, m_e))
 
-        for m in range(m_s, m_e, 1):
-            count += 1
-            filename = "{0}_{1}_{2}.graph".format(args.output, n, m)
-            filepath = os.path.join('outputs/', filename)
-            print(" Count: {} ({}, {}) Filename: {}".format(count, n, m, filename))
+            for m in range(m_s, m_e, 1):
+                count += 1
+                filename = "{0}_{1}_{2}.graph".format(filename_prefix, n, m)
+                filepath = os.path.join('outputs/', filename)
+                print(" Count: {} ({}, {}) Filename: {}".format(count, n, m, filename))
 
-            randomgraph_result = subprocess.run(["./randomgraph", str(n), str(m)],
-                                                encoding='utf-8',
-                                                stdout=subprocess.PIPE)
-            # print(randomgraph_result.stdout)
+                randomgraph_result = subprocess.run(["./randomgraph", str(n), str(m)],
+                                                    encoding='utf-8',
+                                                    stdout=subprocess.PIPE)
+                # print(randomgraph_result.stdout)
 
-            with open(filepath, mode='w', encoding='utf-8') as output_file:
-                output_file.write(randomgraph_result.stdout)
+                with open(filepath, mode='w', encoding='utf-8') as output_file:
+                    output_file.write(randomgraph_result.stdout)
 
 
 if __name__ == '__main__':
