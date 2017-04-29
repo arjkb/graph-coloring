950b3e2ef7769ef56c1d8e15b043ae5e6464c3b1
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-29 13:39:33 -0600

    creating a graph that takes an m value as parameter. Loop counter as seed

--- creategraph.py
+++ creategraph.py
@@ -11,11 +11,12 @@ def main():
     parser.add_argument("n_start", help="number of initial vertices", type=int)
     parser.add_argument("n_finish", help="number of final vertices", type=int)
     parser.add_argument("n_step", help="number of vertices to increment each iteration", type=int)
+    parser.add_argument("m_val", help="number of initial edges m", type=int)
     parser.add_argument("-r", "--runs", help="number of times the entire program must be run", type=int)
     parser.add_argument("--verbose", help="show all output", action="store_true")
     parser.add_argument("--limited", help="show all output", action="store_true")
-    # parser.add_argument("m_start", help="number of initial edges", type=int)
-    # # parser.add_argument("m_finish", help="number of final edges", type=int)
+
+    # parser.add_argument("m_finish", help="number of final edges", type=int)
     # parser.add_argument("m_step", help="number of edges to increment each iteration", type=int)
     # parser.add_argument("-s", "--seed", help="seed value for randomgraph", type=int)
 
@@ -50,31 +51,37 @@ def main():
 
         for n in range(args.n_start, args.n_finish, args.n_step):
             m_max = int(n*(n-1)/2)
-            m_s = int(0.30 * m_max)
-            m_e = int(0.60 * m_max)
+            # m_s = int(0.30 * m_max)
+            # m_e = int(0.60 * m_max)
+
+            m_s = args.m_val
+            # m_e = int(0.60 * m_max)
+
             if args.verbose:
                 print(" For n = {}, m_start={}, m_end={}".format(n, m_s, m_e))
 
-            for m in range(m_s, m_e, 1):
+            # for m in range(m_s, m_e, 1):
+            for m in range(m_s, m_s + 1, 1):
                 count += 1
                 filename = "{0}_{1}_{2}.graph".format(filename_prefix, n, m)
-                filepath = os.path.join('outputs_n100_m1238/', filename)
+                filepath = os.path.join('output_n100_m1238/', filename)
 
                 if args.verbose:
                     print(" Count: {} ({}, {}) Filename: {}".format(count, n, m, filename))
                 elif args.limited and (count % 1000 == 0):
                     print(" Count: {} ({}, {}) Filename: {}".format(count, n, m, filename))
 
-                randomgraph_result = subprocess.run(["./randomgraph", str(n), str(m)],
+                randomgraph_result = subprocess.run(["./randomgraph", str(n), str(m), str(count)],
                                                     encoding='utf-8',
                                                     stdout=subprocess.PIPE)
-                # print(randomgraph_result.stdout)
+                print(randomgraph_result.stdout)
 
                 with open(filepath, mode='w', encoding='utf-8') as output_file:
                     output_file.write(randomgraph_result.stdout)
+                    randomgraph_result = None
 
 
-    print("\n Created {} sets of {} graphs in outputs/ directory"
+    print("\n Created {} sets of {} graphs in output_n100_m1238/ directory"
                                     .format(total_runs, count/total_runs))
     print(" Total number of graphs: {}".format(count))
     print()
