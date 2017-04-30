bc4eec9f497cdd3bf42eaa7a36f441f8c342bc38
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-30 14:59:57 -0600

    function to print the runtimes in descending order

---
+++ get_run_outlier.py
@@ -0,0 +1,34 @@
+import argparse
+import numpy as np
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename",
+                        help="full path to the input file that contains an array",
+                        type=str)
+    parser.add_argument("--ndigits",
+                        help="number of decimal places to round to. (def=10)",
+                        type=int)
+    args = parser.parse_args()
+
+    with open(args.filename, mode='r', encoding='utf-8') as input_file:
+        runtimes = input_file.read().splitlines()
+    runtimes = sorted(list(map(float, runtimes)), reverse=True)
+
+    for r in runtimes:
+        print(r)
+
+
+    decimal = 10
+    if args.ndigits:
+        decimal = args.ndigits
+
+    # print(runtimes, type(runtimes[0]))
+    print(" Variance of {} runtimes: {}".format(len(runtimes),
+                                              np.around(np.var(runtimes, ddof=1), decimal)
+                                              )
+          )
+
+if __name__ == '__main__':
+    main()
