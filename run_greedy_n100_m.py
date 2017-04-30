5a60bfc29bcd473e5968b127f3319d00f30d8e08
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-30 16:09:02 -0600

    taking graphs directory from the command line

--- run_greedy_n100_m.py
+++ run_greedy_n100_m.py
@@ -10,22 +10,25 @@ def is_graph_file(filename):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("filename", help="name of the output file", type=str)
+    # parser.add_argument("filename", help="name of the output file", type=str)
+    parser.add_argument("directory", help="directory which contains the graphs", type=str)
     parser.add_argument("-v", "--verbose", help="show full output", action="store_true")
     parser.add_argument("-l", "--limited", help="show limited output", action="store_true")
     parser.add_argument("-q", "--quiet", help="show no output", action="store_true")
     args = parser.parse_args()
 
     # grab the graph files -- the ones with .graph extension
-    graph_files = filter(is_graph_file, sorted(os.listdir('output_n100_m1238/')))
+    # graph_files = filter(is_graph_file, sorted(os.listdir('output_n100_m1238/')))
+    graph_files = filter(is_graph_file, sorted(os.listdir(args.directory)))
 
-    op_filename = args.filename + ".greedy_time"
-    op_filepath = os.path.join('output_n100_m1238/', op_filename)
+    # op_filename = args.filename + ".greedy_time"
+    # op_filepath = os.path.join('output_n100_m1238/', op_filename)
+    # op_filepath = os.path.join(args.directory, op_filename)
 
     count = 0
     for graph_file in graph_files:
         count += 1
-        graph_filename = os.path.join('output_n100_m1238/', graph_file)
+        graph_filename = os.path.join(args.directory, graph_file)
 
         starttime = datetime.now()
         greedy_result = subprocess.run(["./greedy", graph_filename],
@@ -34,18 +37,18 @@ def main():
         endtime = datetime.now()
 
         total_time = (endtime - starttime).total_seconds()
-        print(total_time)
+        print(total_time, graph_filename)
 
-        if args.verbose:
-            print(" Runcount {}: ./greedy {} >> {}"
-                            .format(count, graph_file, op_filename))
-        elif args.limited:
-            if (count % 1000) == 0:
-                print(" Running count {}: ./greedy {} >> {}"
-                      .format(count, graph_file, op_filename))
+        # if args.verbose:
+        #     print(" Runcount {}: ./greedy {} >> {}"
+        #                     .format(count, graph_file, op_filename))
+        # elif args.limited:
+        #     if (count % 1000) == 0:
+        #         print(" Running count {}: ./greedy {} >> {}"
+        #               .format(count, graph_file, op_filename))
 
-        with open(op_filepath, mode='a', encoding='utf-8') as output_file:
-            output_file.write(str(total_time) + "\n")
+        # with open(op_filepath, mode='a', encoding='utf-8') as output_file:
+        #     output_file.write(str(total_time) + "\n")
             # output_file.write(greedy_result.stdout)
 
 
