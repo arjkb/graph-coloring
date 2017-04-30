aa945f853f52149545a541c49a8a79c7a2354c52
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-30 17:51:36 -0600

    committing master

--- creategraph.py
+++ creategraph.py
@@ -7,33 +7,20 @@ import math
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("output_dir", help="directory to output files to", type=str)
     parser.add_argument("output", help="output file prefix", type=str)
     parser.add_argument("n_start", help="number of initial vertices", type=int)
     parser.add_argument("n_finish", help="number of final vertices", type=int)
     parser.add_argument("n_step", help="number of vertices to increment each iteration", type=int)
-    parser.add_argument("-r", "--runs", help="number of times the entire program must be run", type=int)
     parser.add_argument("--verbose", help="show all output", action="store_true")
-    # parser.add_argument("m_start", help="number of initial edges", type=int)
-    # # parser.add_argument("m_finish", help="number of final edges", type=int)
-    # parser.add_argument("m_step", help="number of edges to increment each iteration", type=int)
-    # parser.add_argument("-s", "--seed", help="seed value for randomgraph", type=int)
-
+    parser.add_argument("--limited", help="show all output", action="store_true")
     args = parser.parse_args()
 
     print(" Output file prefix: ", args.output)
-
     print(" N start: ", args.n_start)
-    # print(" N finish: ", args.n_finish)
+    print(" N finish: ", args.n_finish)
     print(" N step: ", args.n_step)
-
-    # print(" M start: ", args.m_start)
-    # # print(" M finish: ", args.m_finish)
-    # print(" M step: ", args.m_step)
-
-    # if args.seed:
-    #     print(" Seed: ", args.seed)
-    # else:
-    #     print(" Seed NOT present")
+    print(" Creating graphs...")
 
     total_runs = 1
     if args.runs:
@@ -41,38 +28,44 @@ def main():
     else:
         total_runs = 1
 
-    print(" Creating graphs...")
-
     count = 0
     for run_count in range(total_runs):
         filename_prefix = args.output + str(run_count)
 
-        for n in range(args.n_start, args.n_finish, args.n_step):
+        for n in range(args.n_start, args.n_finish + 1, args.n_step):
             m_max = int(n*(n-1)/2)
-            m_s = int(0.30 * m_max)
-            m_e = int(0.60 * m_max)
-            if args.verbose:
-                print(" For n = {}, m_start={}, m_end={}".format(n, m_s, m_e))
+            m_start = int(0.10 * m_max)
+            m_10perc = int(0.10 * m_max)
+            m_end = int(1.00 * m_max)
 
-            for m in range(m_s, m_e, 1):
+            if args.verbose:
+                print(" For n = {}, m_start={}, m_end={}".format(n, m_start, m_end))
+
+            m_count = 0
+            for m in range(m_start, m_end + 1, m_10perc):
                 count += 1
-                filename = "{0}_{1}_{2}.graph".format(filename_prefix, n, m)
-                filepath = os.path.join('outputs/', filename)
+                m_count += 1
+
+                filename = "{0}_n{1}_m{2}.graph".format(filename_prefix, n, m)
+                filepath = os.path.join(args.output_dir, filename)
 
                 if args.verbose:
                     print(" Count: {} ({}, {}) Filename: {}".format(count, n, m, filename))
+                elif args.limited and (count % 1000 == 0):
+                    print(" Count: {} ({}, {}) Filename: {}".format(count, n, m, filename))
 
-                randomgraph_result = subprocess.run(["./randomgraph", str(n), str(m)],
+                randomgraph_result = subprocess.run(["./randomgraph", str(n), str(m), str(count)],
                                                     encoding='utf-8',
                                                     stdout=subprocess.PIPE)
-                # print(randomgraph_result.stdout)
 
                 with open(filepath, mode='w', encoding='utf-8') as output_file:
                     output_file.write(randomgraph_result.stdout)
+                    randomgraph_result = None
+                if m_count == 10:
+                    break
 
-
-    print("\n Created {} sets of {} graphs in outputs/ directory"
-                                    .format(total_runs, count/total_runs))
+    print("\n Created {} sets of {} graphs in {}/ directory"
+                                    .format(total_runs, count/total_runs, args.output_dir))
     print(" Total number of graphs: {}".format(count))
     print()
 
--- greedy.c
+++ greedy.c
@@ -3,157 +3,157 @@
 #define MAXNODES 100
 #define MAXEDGES 100*(100-1)
 
-/* GREEDY    Graph coloring algorithm C.C. McGeoch 2011 
- 
+/* GREEDY    Graph coloring algorithm C.C. McGeoch 2011
    Usage: greedy filename
 
    To report counts:       #define REPORTSTATS  1
    To report the coloring: #define REPORTCOLORS 1
-*/ 
-#define REPORTSTATS  1
-#define REPORTCOLORS 1
+*/
+// #define REPORTSTATS  1
+// #define REPORTCOLORS 1
+// #define REPORYSUMMARY 1
 
 int numnodes=0;    /* number of nodes in the graph */
-int numedges=0; 
+int numedges=0;
 int checkcount=0;  /* number of colors checked */
-int colorcount=0;  /* to compute average colors looked at */ 
-int maxcolor=0;    /* max color assigned */ 
+int colorcount=0;  /* to compute average colors looked at */
+int maxcolor=0;    /* max color assigned */
 
-/* The graph is represented as an adjacency list in two arrays */ 
-int edgex[MAXNODES+1];      /* index to neighbor array */ 
-int ecount[MAXNODES+1];     /* number of neighbors this node */ 
-int neighbor[MAXEDGES+1];   /* adjacent nodes       */ 
+/* The graph is represented as an adjacency list in two arrays */
+int edgex[MAXNODES+1];      /* index to neighbor array */
+int ecount[MAXNODES+1];     /* number of neighbors this node */
+int neighbor[MAXEDGES+1];   /* adjacent nodes       */
 
-int colorof[MAXNODES+1];    /* node colors */  
+int colorof[MAXNODES+1];    /* node colors */
 
-int colorcounts[MAXNODES+1]; 
+int colorcounts[MAXNODES+1];
 
 FILE *fp, *fopen();
 
-/*--------------------------------------------------------------*/ 
-/* getgraph: Read input file and build graph */ 
+/*--------------------------------------------------------------*/
+/* getgraph: Read input file and build graph */
 /*  It works if only edge (5,3) of both (5,3), (3,5) are input, since
-    it only checks lower-numbered neighbors of 5.   */ 
-/* Input Format:  
-      First line is integer number of nodes. 
-      Then: 
+    it only checks lower-numbered neighbors of 5.   */
+/* Input Format:
+      First line is integer number of nodes.
+      Then:
          neighbors of v1, list ending with 0  (none < 1)
-         neighbors of v2, list ending with 0  
-         ... 
-         neighbors of vn, list ending with 0  (could be 1..n-1) 
+         neighbors of v2, list ending with 0
+         ...
+         neighbors of vn, list ending with 0  (could be 1..n-1)
       Lines after n+1 inputs are ignored as comments.
 
 */
 void getgraph()
 {
-  int i;   
-  int edgeindex = 1; 
-  int edgecount; 
-  int nodeval; 
+  int i;
+  int edgeindex = 1;
+  int edgecount;
+  int nodeval;
 
     fscanf(fp,"%d", &numnodes);  /* number of nodes in graph */
     if (numnodes >MAXNODES) {
       printf("too many nodes: recompile and try again\n");
-      exit(1); 
+      exit(1);
     }
-    
-    for (i=1; i <= numnodes; i++) { 
-      edgex[i] = edgeindex; /* where it starts in edge array */ 
-      
-      fscanf(fp, "%d", &nodeval); 
-      edgecount=0; 
-    
-      while (nodeval != 0) {  
+
+    for (i=1; i <= numnodes; i++) {
+      edgex[i] = edgeindex; /* where it starts in edge array */
+
+      fscanf(fp, "%d", &nodeval);
+      edgecount=0;
+
+      while (nodeval != 0) {
 	numedges++;         /* total for accounting */
-        edgecount++;        /* total neighbors this node */ 
+        edgecount++;        /* total neighbors this node */
 
-        neighbor[edgeindex++] = nodeval; 
+        neighbor[edgeindex++] = nodeval;
 
-	fscanf(fp, "%d", &nodeval);        
+	fscanf(fp, "%d", &nodeval);
 
       } /* for each adjacent node */
-      ecount[i] = edgecount; 
+      ecount[i] = edgecount;
 
-    } /* for each node in graph */ 
+    } /* for each node in graph */
 
-} /* end getgraph */         
+} /* end getgraph */
 
 
 /*--------------------------------------------------------------*/
-/* check if this color is valid 0 no, 1 yes                     */ 
+/* check if this color is valid 0 no, 1 yes                     */
 
 int checkcolorok(int thenode, int thecolor)
 {
-  int ex = edgex[thenode];  /* index to neighbor list */ 
-  int edges = ecount[thenode]; /* number of neighbors */ 
+  int ex = edgex[thenode];  /* index to neighbor list */
+  int edges = ecount[thenode]; /* number of neighbors */
 
-  int e=1;     
+  int e=1;
 
   while (e++ <= edges){
-    checkcount++; 
-    if ( (neighbor[ex] <= thenode) && 
-         (colorof [neighbor[ex++] ] == thecolor)) return 0; /* false */ 
+    checkcount++;
+    if ( (neighbor[ex] <= thenode) &&
+         (colorof [neighbor[ex++] ] == thecolor)) return 0; /* false */
   }
- 
-  return  1; 
+
+  return  1;
 }
 
-/*--------------------------------------------------------------*/ 
-void colorit (int thenode, int thecolor) 
+/*--------------------------------------------------------------*/
+void colorit (int thenode, int thecolor)
 {
   colorof[thenode] = thecolor;
-  colorcounts[thecolor]++; 
-}   
+  colorcounts[thecolor]++;
+}
 
-/*--------------------------------------------------------------*/ 
+/*--------------------------------------------------------------*/
 void colorgraph()
 {
-    
-  int v ;
-  int k; 
 
-  for (k = 1; k <= numnodes; k++) colorcounts[k] = 0; /* color distribuion*/ 
+  int v ;
+  int k;
+
+  for (k = 1; k <= numnodes; k++) colorcounts[k] = 0; /* color distribuion*/
 
   for (v = 1; v <= numnodes; v++) {
     for (k = 1; k <= numnodes; k++ ) {
-      colorcount++;  /* number of colors examined*/  
+      colorcount++;  /* number of colors examined*/
       if (checkcolorok(v, k)) {
 	colorit(v, k);
-        if (k > maxcolor) maxcolor=k; 
+        if (k > maxcolor) maxcolor=k;
 	break;
     }
     }//for k
   }//for v
 }
 /*-----------------------------------------------------------*/
-void printcolors() 
+void printcolors()
 {
-  int v; 
+  int v;
   for (v =1; v<= numnodes; v++) {
-    printf("%d %d\t", v, colorof[v]); 
-    if (v % 10 == 0) printf("\n"); 
+    printf("%d %d\t", v, colorof[v]);
+    if (v % 10 == 0) printf("\n");
   }
 }
 
-/*-----------------------------------------------------------*/ 
-void printgraph() 
+/*-----------------------------------------------------------*/
+void printgraph()
 {
   int src;
-  int ex; 
-  int i; 
+  int ex;
+  int i;
 
- 
+
   for (src = 1; src <= numnodes; src++) {
     ex = edgex[src];
-    printf("\n v %d: ", src); 
+    printf("\n v %d: ", src);
     for (i=1; i <= ecount[src]; i++) {
       printf("%d \t", neighbor[ex++]);
     }
   }
-  printf("\n"); 
+  printf("\n");
 }
 
-/*-----------------------------------------------------------*/ 
+/*-----------------------------------------------------------*/
 main(argc, argv)
 int argc;
 char *argv[];
@@ -171,27 +171,29 @@ char *argv[];
 		    getgraph();
 		    colorgraph();
 #ifdef REPORTCOLORS
-                    printf("vertex color pairs:\n"); 
+                    printf("vertex color pairs:\n");
                     printcolors();
-#endif 
+#endif
 
-#ifdef REPORTSTATS  
-                    printf("graph:\n"); 
-                    printgraph(); 
+#ifdef REPORTSTATS
+                    printf("graph:\n");
+                    printgraph();
+#endif
 		    double avecolor= ((double) colorcount) / (double)numnodes;
-                    printf("n  m  compares maxcolor  colorcount ave: \n");
-		    printf("%d %d    %d      %d       %d         %lf\n", 
-			   numnodes, numedges,
-			   checkcount, maxcolor, colorcount, avecolor);
+                    // printf("n  m  compares maxcolor  colorcount ave: \n");
+		    // printf("%d %d    %d      %d       %d         %lf\n",
+			  //  numnodes, numedges,
+			  //  checkcount, maxcolor, colorcount, avecolor);
+
+#ifdef REPORTSTATS
 		    int i;
-                    printf("color counts:\n"); 
+                    printf("color counts:\n");
 		    for (i=1; i <= numnodes; i++) {
 		      printf("%d %d \t", i, colorcounts[i]);
 		      if (i % 10 == 0) printf("\n");
 		      if (colorcounts[i]==0) break;
 		    }
-		    		    
 #endif
-		  }  /* end else */ 
-	} /* end else */ 
-} /* end main */ 
+		  }  /* end else */
+	} /* end else */
+} /* end main */
