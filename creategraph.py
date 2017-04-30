338525a0d1623b56bf925bd6add2d789ea7e6b80
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-04-30 17:22:07 -0600

    dry runs of graph generation

--- creategraph.py
+++ creategraph.py
@@ -11,7 +11,7 @@ def main():
     parser.add_argument("n_start", help="number of initial vertices", type=int)
     parser.add_argument("n_finish", help="number of final vertices", type=int)
     parser.add_argument("n_step", help="number of vertices to increment each iteration", type=int)
-    parser.add_argument("m_val", help="number of initial edges m", type=int)
+    # parser.add_argument("m_val", help="number of initial edges m", type=int)
     parser.add_argument("-r", "--runs", help="number of times the entire program must be run", type=int)
     parser.add_argument("--verbose", help="show all output", action="store_true")
     parser.add_argument("--limited", help="show all output", action="store_true")
@@ -25,7 +25,7 @@ def main():
     print(" Output file prefix: ", args.output)
 
     print(" N start: ", args.n_start)
-    # print(" N finish: ", args.n_finish)
+    print(" N finish: ", args.n_finish)
     print(" N step: ", args.n_step)
 
     # print(" M start: ", args.m_start)
@@ -46,23 +46,25 @@ def main():
     print(" Creating graphs...")
 
     count = 0
+
     for run_count in range(total_runs):
         filename_prefix = args.output + str(run_count)
 
-        for n in range(args.n_start, args.n_finish, args.n_step):
+        for n in range(args.n_start, args.n_finish + 1, args.n_step):
             m_max = int(n*(n-1)/2)
-            # m_s = int(0.30 * m_max)
-            # m_e = int(0.60 * m_max)
-
-            m_s = args.m_val
-            # m_e = int(0.60 * m_max)
+            m_start = int(0.10 * m_max)
+            m_10perc = int(0.10 * m_max)
+            m_end = int(1.00 * m_max)
 
             if args.verbose:
-                print(" For n = {}, m_start={}, m_end={}".format(n, m_s, m_e))
+                print(" For n = {}, m_start={}, m_end={}".format(n, m_start, m_end))
 
             # for m in range(m_s, m_e, 1):
-            for m in range(m_s, m_s + 1, 1):
+            m_count = 0
+            for m in range(m_start, m_end + 1, m_10perc):
                 count += 1
+
+
                 filename = "{0}_{1}_{2}.graph".format(filename_prefix, n, m)
                 filepath = os.path.join('output_n100_m1238/', filename)
 
@@ -74,11 +76,14 @@ def main():
                 randomgraph_result = subprocess.run(["./randomgraph", str(n), str(m), str(count)],
                                                     encoding='utf-8',
                                                     stdout=subprocess.PIPE)
-                print(randomgraph_result.stdout)
+                m_count += 1
+                if m_count == 10:
+                    break
+                    # print(randomgraph_result.stdout)
 
-                with open(filepath, mode='w', encoding='utf-8') as output_file:
-                    output_file.write(randomgraph_result.stdout)
-                    randomgraph_result = None
+                    # with open(filepath, mode='w', encoding='utf-8') as output_file:
+                    #     output_file.write(randomgraph_result.stdout)
+                    #     randomgraph_result = None
 
 
     print("\n Created {} sets of {} graphs in output_n100_m1238/ directory"
