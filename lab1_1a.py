5a7156ff41d6c0e4b64a70f46bb695b07a7d436e
Joe Renzullo <joerenzullo@gmail.com>
2017-05-05 16:37:31 -0600

    fixed bug in handling of random

--- lab1_1a.py
+++ lab1_1a.py
@@ -39,7 +39,7 @@ def run_random_100_times(iterations):
                 colors = ''
                 path = 'graphs_to_test/' + filename.strip()
                 start_time = datetime.now()
-                subprocess.run(["./random " + path + ' ' + str(iteration**2) + " > " +
+                subprocess.run(["./random " + path + ' ' + str(2**iteration) + " > " +
                                 filename + '.results'], shell=True)
                 end_time = datetime.now()
                 with open(filename + '.results', 'r') as in_file:
@@ -49,8 +49,8 @@ def run_random_100_times(iterations):
                 total_time = (end_time - start_time).total_seconds()
                 data[filename] = {'time': total_time, 'colors': colors}
             trials[i] = data
-
+        iter_data['iteration_' + str(iteration)] = trials
         with open('lab1_1a_random_results.json', 'w') as out_file:
-            json.dump(trials, out_file)
+            json.dump(iter_data, out_file)
 
-run_random_100_times(4)
+run_random_100_times(7)
