b3c2f7490be436a86d845a4e8c4e1fcc9711cf25
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-05-04 20:46:31 -0600

    cleaning up the program

--- cweight_test_analysis.py
+++ cweight_test_analysis.py
@@ -8,28 +8,19 @@ def get_output_filepath(directory, filename):
 
 is_graph_file = lambda filename: filename.endswith('.graph')
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("inp_dir", help="graph file directory to process", type=str)
     parser.add_argument("out_dir", help="output directory", type=str)
     args = parser.parse_args()
 
-    print(args.inp_dir)
-    print(args.out_dir)
+    print(" Input directory: ", args.inp_dir)
+    print(" Output directory: ", args.out_dir)
 
     gr_files_list = filter(is_graph_file, sorted(os.listdir(args.inp_dir)))
 
-    # i = 0
-    # for f in gr_files_list:
-    #     print(i, f)
-    #     i += 1
-
-    i = 0
-    file_count = 0
     for gr_file_name in gr_files_list:
-        # read the 100 tuples
-        # Find median
-        file_count += 1
         gr_file_path = os.path.join(args.inp_dir, gr_file_name)
         with open(gr_file_path, mode='r', encoding='utf-8') as graph_file:
             lines = graph_file.readlines()
@@ -37,22 +28,15 @@ def main():
             for line in lines:
                 # each line is comma-separated. Get the first value of those
                 a_list.append(float(line.strip().split(',')[0]))
-            # print(a_list)
             average = numpy.average(a_list)
             std_dev = numpy.std(a_list, ddof=1)
             variance = numpy.var(a_list, ddof=1)
             median = numpy.median(a_list)
-
             summary_line = "{} {} {} {}".format(average, median, std_dev, variance)
 
             op_file_path = get_output_filepath(args.out_dir, gr_file_name.replace('.graph', '.summary'))
             with open(op_file_path, mode='w', encoding='utf-8') as output_file:
                 output_file.write(summary_line)
-        # print(type(lines), len(lines))
-
-
-    print()
-    print(file_count)
 
 
 if __name__ == '__main__':
