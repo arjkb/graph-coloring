c83feb957851b2b997eaef2d256147c6b2fb37ef
Austin Short <austinshort1@gmail.com>
2017-05-06 17:01:18 -0600

    added graphs for iter vs color for fixed n

--- plots/.#lab1_1_colorplot4_random.py
+++
@@ -1,1 +0,0 @@
-ashort@deimos.cs.unm.edu.11372:1493235710
--- plots/lab1_1_colorplot4_random.py
+++ plots/lab1_1_colorplot4_random.py
@@ -11,20 +11,24 @@ with open("lab1_1_random_summary_stats.txt") as input_file:
     trials = input_file.read().splitlines()
 
 
-n = [10,40, 50, 100]
+n = 100
 
-NUM_COLORS = len(n)
+NUM_COLORS = 10
 
-colors = [[] for y in range(len(n))]
-iterations = [[] for y in range(len(n))]
+colors = [[]for x in range(10)]
+iterations = [[]for x in range(10)]
 
-for y in range (0, (len(n))):
-    for x in range (0, 1200):
-        iter = trials[x].split(" ")
+for x in range (0, 1200):
+    iter = trials[x].split(" ")
         
-        if (int(iter[0]) == int(n[y])):
-            colors[y].append(float(iter[2]))
-            iterations[y].append(int(iter[6]))
+    if (int(iter[0]) == n):
+        nodes = int(iter[0])
+        maxedges = float(nodes*(nodes-1))/2
+        edges = float(iter[1])
+        percent = (edges/maxedges)*100
+        index = (roundup(percent)/10) -1
+        colors[index].append(float(iter[2]))
+        iterations[index].append(int(iter[6]))
 
 
 
@@ -32,17 +36,16 @@ cm = plt.get_cmap('gist_rainbow')
 fig = plt.figure(facecolor='white')
 ax = fig.add_subplot(111)
 ax.set_color_cycle([cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)])
-ax.set_ylim(0,20)
 ax.set_xlim(0,12)
 
-for x in range (0, len(n)):
+for x in range (0, 10):
     plt.scatter(iterations[x], colors[x], color=(cm(1.*x/NUM_COLORS)))
-    plt.plot(iterations[x], colors[x],label = "n="+str(n[x])+" m="+str(m[x]))
+    plt.plot(iterations[x], colors[x],label = "m="+str((x+1)*10) + "%")
     #ax.plot(np.unique(iterations[x]), np.poly1d(np.polyfit(iterations[x], time[x], 1))(np.unique(m[x])), label = "n="+str((x+1)*10))
 
 
-plt.suptitle('Number of colors vs Iterations')
+plt.suptitle('Number of colors vs Iterations\nFor n = '+str(n))
 plt.xlabel('log$2$ of Number of Iterations')
 plt.ylabel('Number of colors')
-plt.legend(loc=2)
+plt.legend(loc=1)
 plt.show();
