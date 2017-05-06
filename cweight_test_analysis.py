f232cdfb703c3ae289904eefe95168e231c5263b
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-05-05 22:10:37 -0600

    store different categories of cweights, and the files in each category, into a dictionary

--- cweight_test_analysis.py
+++ cweight_test_analysis.py
@@ -1,12 +1,25 @@
 import argparse
 import os
 import re
+import itertools
 
 import numpy
 
 
 def get_output_filepath(directory, filename):
     return os.path.join(directory, filename)
+
+
+def get_file_average(filename):
+    with open(filename, mode='r', encoding='utf-8') as file:
+        lines = file.readlines()
+        runtimes = list()
+        print("Opening!")
+        for line in lines:
+            runtimes.append(float(line.strip().split(',')[0]))
+        average_runtime = numpy.average(runtimes)
+    return average_runtime
+
 
 is_graph_file = lambda filename: filename.endswith('.graph')
 
@@ -24,51 +37,39 @@ def main():
 
     gr_files_list = filter(is_graph_file, sorted(os.listdir(args.inp_dir)))
 
+    gr_files_list_it1, gr_files_list_it2 = itertools.tee(gr_files_list, 2)
+
+    # find all the categories
+    # for each category
+    ## grab all the files in the category
+    ## find the averages of all runtimes for each file in a category
+    ## -- save it into a list
+    ## find the
 
     count = 0
     new_pattern = ""
     current_pattern = ""
     cweights_file_categories = list()
-    for gr_file_name in gr_files_list:
+
+    categorized_files = dict()
+    for gr_file_name in gr_files_list_it1:
         m = re.search(pattern, gr_file_name)
         if m is not None:
             new_pattern = m.group(0)
             if new_pattern != current_pattern:
                 cweights_file_categories.append(new_pattern)
                 current_pattern = new_pattern
-                # print(current_pattern)
 
-    for cat in cweights_file_categories:
-        print(cat)
+            if new_pattern not in categorized_files:
+                categorized_files[new_pattern] = list()
 
-        # print(gr_file_name)
+            categorized_files[new_pattern].append(gr_file_name)
 
+    for k in categorized_files.keys():
+        print(" CATEGORY :", k)
 
-
-        # if m is not None:
-        #     current_pattern = m.group(0)
-        #     print(current_pattern)
-        # print(gr_file_name)
-        # count += 1
-        # print(count)
-
-    # for gr_file_name in gr_files_list:
-    #     gr_file_path = os.path.join(args.inp_dir, gr_file_name)
-    #     with open(gr_file_path, mode='r', encoding='utf-8') as graph_file:
-    #         lines = graph_file.readlines()
-    #         a_list = list()
-    #         for line in lines:
-    #             # each line is comma-separated. Get the first value of those
-    #             a_list.append(float(line.strip().split(',')[0]))
-    #         average = numpy.average(a_list)
-    #         std_dev = numpy.std(a_list, ddof=1)
-    #         variance = numpy.var(a_list, ddof=1)
-    #         median = numpy.median(a_list)
-    #         summary_line = "{} {} {} {}".format(average, median, std_dev, variance)
-    #
-    #         op_file_path = get_output_filepath(args.out_dir, gr_file_name.replace('.graph', '.summary'))
-    #         with open(op_file_path, mode='w', encoding='utf-8') as output_file:
-    #             output_file.write(summary_line)
+        for v in categorized_files[k]:
+            print(v)
 
 
 if __name__ == '__main__':
